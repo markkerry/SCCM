126fef4152fd13a3f91133ceb91fcb70ea81a295
markkerry <mark-kerry@outlook.com>
2016-12-15 11:12:50 +0000

    Update Set-CCMCacheLimit.ps1

--- Set-CCMCacheLimit.ps1
+++ Set-CCMCacheLimit.ps1
@@ -42,7 +42,7 @@
         # Check for elevation
         if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
             Write-Warning "You need to run this script from an elevated PowerShell prompt!`nPlease start PowerShell as an Administrator and re-run the script..."
-        break
+            break
         }
     }
 
