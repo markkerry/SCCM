15de9d1f357c4a5d2a6d9afc0156b1970f56306f
Mark Kerry <mark-kerry@outlook.com>
2018-08-30 12:40:53 +0100

    Delete OneDriveSyncUserRemediateV2.ps1

--- OneDriveSyncUserRemediateV2.ps1
+++
@@ -1,9 +0,0 @@
-$oneDriveDir = "$ENV:LOCALAPPDATA\Microsoft\OneDrive\"
-$oneDriveExe = "OneDrive.exe"
-
-try {
-    Start-Process (Join-Path -Path $oneDriveDir -ChildPath $oneDriveExe)
-}
-catch {
-    Write-Output "Failed to create $oneDriveExe in $oneDriveDir"
-}
